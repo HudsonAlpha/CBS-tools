b637b6ceb8746e154f452cd90bd7e110d9021540
Lauren <lhartleywhitt@gmail.com>
2026-08-28 19:32:23 -0400

    Update and rename CBS_Phaser.sh to CBS_Phase.sh
    Renaming Phaser to Phase

--- CBS_Phase.sh
+++ CBS_Phase.sh
@@ -2,14 +2,14 @@
 #
 # requires samtools and bwa-mem, R plotting is optional
 ################################################################################
-#                           CBS tools: CBS Phaser.                             #
+#                           CBS tools: CBS Phase.                             #
 #         Sex chromosome phasing between haplotypes of genome assembly         #
 ################################################################################
 #                       0. Reading command-line input                          #
 ################################################################################
 
 #Optional
-output='./CBS_Phaser_out'
+output='./CBS_Phase_out'
 t_num=1
 ref_genomes=()
 redo=0
@@ -35,7 +35,7 @@ print_usage() {
 	echo "Mapping Options:"
 	echo "	-s  [Male|Female] Sex-specific k-mers to map (case sensitive)"
 	echo "			Ideally set to the heterogametic sex determined with Discovery."
-	echo "			If results were inconclusive, Phaser can map both sets of sex-specific k-mers by omitting this option (DEFAULT)."
+	echo "			If results were inconclusive, Phase can map both sets of sex-specific k-mers by omitting this option (DEFAULT)."
 	echo
 	echo "	-t	Number of threads to use, DEFAULT: 1"
 	echo 
@@ -50,15 +50,15 @@ print_usage() {
 	echo "	-p	Prefix to add to plot file names, DEFAULT: kmer_hist"
 	echo
 	echo "General Options:"
-	echo "	-o  Output directory path and name, DEFAULT: ./CBS_Phaser_out"
+	echo "	-o  Output directory path and name, DEFAULT: ./CBS_Phase_out"
 	echo
 	echo "	-r	Redo/overwrite results in output directory"
-	echo "			CBS Phaser's default behavior won't overwrite files of the same name in output directory."
-	echo "			If additional k-mer lists are added to an existing Phaser run, it will skip existing outputs and only run on new inputs." 
+	echo "			CBS Phase's default behavior won't overwrite files of the same name in output directory."
+	echo "			If additional k-mer lists are added to an existing Phase run, it will skip existing outputs and only run on new inputs." 
 	echo "			If an error occured during the job, it would be best to use -r or delete the suspect files manually and rerun without -r."
 }
 
-date "+%c Starting CBS Phaser"
+date "+%c Starting CBS Phase"
 
 while getopts ':d:g:s:t:k:b:m:p:o:rHh' flag
 do
@@ -139,7 +139,7 @@ do
 		echo "Existing BWA index files found for ${assem}! Skipping to BWA MEM"
 		echo "Use -r to overwrite BWA index files"
 	elif [ -s "${assem}.amb" ] && [ -s "${assem}.ann" ] && [ -s "${assem}.bwt" ] && [ -s "${assem}.pac" ] && [ -s "${assem}.sa" ] && [ "$redo" = 1 ]; then
-		echo "Existing BWA index files found for ${assem}! WARNING: redo (-r) set - CBS Phaser will overwrite these files!"
+		echo "Existing BWA index files found for ${assem}! WARNING: redo (-r) set - CBS Phase will overwrite these files!"
 		bwa index ${assem}
 	else
 		bwa index ${assem}
@@ -165,10 +165,10 @@ do
 			date "+%c ----- $cf"
 
 			if [ -s "${output}/${hap}/${cf}_scaffolds.bam" ] && [ "$redo" = 0 ]; then
-				echo "Existing k-mer mapping depth file found! Looks like Phaser successfully finished mapping this k-mer list, moving on to next input"
+				echo "Existing k-mer mapping depth file found! Looks like Phase successfully finished mapping this k-mer list, moving on to next input"
 				echo "Use -r to redo k-mer mapping"
 			elif [ -s "${output}/${hap}/${cf}_scaffolds.bam" ] && [ "$redo" = 1 ]; then
-				echo "Existing k-mer mapping depth file found! WARNING: redo (-r) set - CBS Phaser will overwrite mapping output for this k-mer list!"
+				echo "Existing k-mer mapping depth file found! WARNING: redo (-r) set - CBS Phase will overwrite mapping output for this k-mer list!"
 				bwa mem -t $t_num -k $k -T $k -a -c 10 $assem kmer_lists/$cf.fasta > ${output}/${hap}/${cf}_scaffolds.sam
 				samtools sort -@ $t_num ${output}/${hap}/${cf}_scaffolds.sam -o ${output}/${hap}/${cf}_scaffolds.bam
 				rm ${output}/${hap}/${cf}_scaffolds.sam
@@ -197,7 +197,7 @@ do
 	if [ "$histogram_plots" = 1 ]; then
 		date "+%c --- Creating quick plot to view k-mer peaks on ${hap} ---"
 		echo
-		Rscript "${SCRIPT_DIR}"/src/Phaser_kmer_plotting.R -r "${output}/${hap}.chromSizes" -p "${output}/${hap}" -o "${output}/${plot_prefix}.${hap}" -b "${bin_size}" -m ${min_size}
+		Rscript "${SCRIPT_DIR}"/src/Phase_kmer_plotting.R -r "${output}/${hap}.chromSizes" -p "${output}/${hap}" -o "${output}/${plot_prefix}.${hap}" -b "${bin_size}" -m ${min_size}
 	fi
 done
 
